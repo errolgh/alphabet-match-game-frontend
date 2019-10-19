fbff54726bf96d0e6d4332bb33d1e3f41b66da31
errolgh <ewatsonrei@gmail.com>
2019-10-19 17:53:53 -0400

    usable hash

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Title from './Title'
+
+function App() {
+  return (
+    <React.Fragment>
+      <Title/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Title.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Title extends React.Component {
+  render(){
+    return (
+      <React.Fragment>
+        <div className="">
+          <h1>Cyrllic Alphabet</h1>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+
+export default Title
