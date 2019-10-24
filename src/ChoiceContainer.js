f07e4fda3a714fd7c4bdfb2fb57ffc7903fb4a37
errolgh <ewatsonrei@gmail.com>
2019-10-24 08:16:49 -0400

    still crashing

--- src/ChoiceContainer.js
+++ src/ChoiceContainer.js
@@ -1,29 +1,26 @@
 import React from 'react'
 import Choice from './Choice'
 
-export default class ChoiceContainer extends React.Component {
-  render(){
-    return(
-      <form className="ui form">
-        <div className="ui two column centered grid container">
-          {this.props.choices.map((choice) => {
-            return <Choice
-            key={choice.id}
-            choice={choice}
-            updateSelectedChoice={this.props.updateSelectedChoice}
-            />})
-          }
-          <div>
-          {/* the button should be greyed out until a choice is made */}
-            <button
-            className={`ui button green submit-button huge`}
-            type="submit"
-            onClick={(event)=>{this.props.handleSubmit(event)}}
-            > Submit
-            </button>
-          </div>
+export default function ChoiceContainer(props){
+  return(
+    <form className="ui form">
+      <div className="ui two column centered grid container">
+        {props.choices.map((choice) => {
+          return <Choice
+          key={choice.id}
+          choice={choice}
+          updateSelectedChoice={props.updateSelectedChoice}
+          />})
+        }
+        <div>
+        {/* the button should be greyed out until a choice is made */}
+          <button
+          className={`ui button green submit-button huge`}
+          onClick={(event)=>{props.handleSubmit(event)}}
+          > Submit
+          </button>
         </div>
-      </form>
-    )
-  }
+      </div>
+    </form>
+  )
 }
--- src/QuestionLetter.js
+++ src/QuestionLetter.js
@@ -1,13 +1,7 @@
 import React from 'react'
 
-function QuestionLetter(props) {
-  return(
-    <div className="ui centered">
-        <p id="question-letter">
-        {props.currentLetter.character}
-        </p>
-    </div>
-  )
+export default function QuestionLetter(props) {
+  return <div className="ui centered">
+          <p id="question-letter">{props.currentLetter.character}</p>
+         </div>
 }
-
-export default QuestionLetter
