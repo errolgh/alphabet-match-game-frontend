82828f25f3dfb697d2a488412350e8050e14dcaf
errolgh <ewatsonrei@gmail.com>
2019-10-24 07:29:13 -0400

    1 > console error - function QuestionLetter

--- src/QuestionLetter.js
+++ src/QuestionLetter.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
-class QuestionLetter extends React.Component {
-  render(){
-    return(
-      <div className="ui centered">
-          <p id="question-letter">
-          {this.props.currentLetter.character}
-          </p>
-      </div>
-    )
-  }
+function QuestionLetter(props) {
+  return(
+    <div className="ui centered">
+        <p id="question-letter">
+        {props.currentLetter.character}
+        </p>
+    </div>
+  )
 }
 
 export default QuestionLetter
